ebb12d48028e2c3c17fce3dcfb1d9b1fdb9c1b66
Nickolas <nickolas@pop-os.localdomain>
2020-07-17 15:51:27 -0300

    basic classroom implementation

--- OnPlan/OnPlan/urls.py
+++ OnPlan/OnPlan/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('',include('main.urls')),
+    path('',include('login.urls')),
+    path('classroom/',include('classroom.urls')),
     path('admin/', admin.site.urls),
 ]
